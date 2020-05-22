a12c9e839791039de8ac1d2f0b9a03d5cfaa90d6
Olga <fordoing18@gmail.com>
2020-05-22 12:59:14 +0300

    Resume-22

--- resume/app/js/scripts.js
+++ resume/app/js/scripts.js
@@ -43,6 +43,22 @@ $(document).ready(function() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 
   $('.dropdown').on("click", '.toggle', function (e) {
