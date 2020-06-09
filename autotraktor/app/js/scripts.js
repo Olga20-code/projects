350fc1d4481e8a4dd4060f9ed4cb48a7c87820b0
Olga <fordoing18@gmail.com>
2020-06-09 10:50:37 +0300

    autotr-one

--- autotraktor/app/js/scripts.js
+++ autotraktor/app/js/scripts.js
@@ -44,7 +44,32 @@ $(document).ready(function() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
-    dots: true
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 
   let totalSum = $('#total-sum');
