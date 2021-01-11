c3ba0378e41a5a1771a96bbdc830559a01cbe387
Olga20-code <51793152+Olga20-code@users.noreply.github.com>
2021-01-11 17:57:47 +0200

    change menu

--- dentists-newimage/app/js/scripts.js
+++ dentists-newimage/app/js/scripts.js
@@ -82,6 +82,14 @@ $(document).ready(function() {
       {
         breakpoint: 800,
         settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           dots: true
@@ -108,6 +116,14 @@ $(document).ready(function() {
       {
         breakpoint: 850,
         settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           dots: true
@@ -126,23 +142,15 @@ $(document).ready(function() {
         breakpoint: 1024,
         settings: {
           slidesToShow: 5,
-          slidesToScroll: 3,
+          slidesToScroll: 4,
           dots: true
         }
       },
       {
         breakpoint: 800,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToShow: 5,
+          slidesToScroll: 3,
           dots: true
         }
       }
